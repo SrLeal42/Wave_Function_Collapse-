eb629b5efc5bd4bf402a7b67569d7a3b7e1ec78f
SrLeal42 <gabriellealjoao@gmail.com>
2025-11-11 07:27:25 -0300

    REFACTOR: Refatorando o Material Manager

--- wfc/src/scripts/managers/MaterialManager.ts
+++ wfc/src/scripts/managers/MaterialManager.ts
@@ -17,232 +17,58 @@ export class MaterialManager{
 
         this.scene = scene;
 
-        this.materials.set('defaultUnlit', this.CreateDefaultUnLitMaterial());
+        this.materials.set('defaultUnlit', this.CreateSimpleUnlitMaterial('Default_Unlit_Mat', new B.Color3(1, 1, 1)));
 
         // --- GRASSLAND
-        this.materials.set('grassUnlit', this.CreateGrassUnLitMaterial());
-        this.materials.set('grassWetUnlit', this.CreateGrassWetUnLitMaterial());
-        this.materials.set('waterUnlit', this.CreateWaterUnLitMaterial());
-        this.materials.set('sandUnlit', this.CreateSandUnLitMaterial());
-        this.materials.set('stoneUnlit', this.CreateStoneUnLitMaterial());
-        this.materials.set('lowMountainUnlit', this.CreateLowMountainUnLitMaterial());
-        this.materials.set('highMountainUnlit', this.CreateHighMountainUnLitMaterial());
-        this.materials.set('snowUnlit', this.CreateSnowUnLitMaterial());
-        this.materials.set('PlayerDefaultUnlit', this.CreatePlayerDefaultUnLitMaterial());
+        this.materials.set('grassUnlit', this.CreateSimpleUnlitMaterial('Grass_Unlit_Mat', new B.Color3(0, 1, 0)));
+        this.materials.set('grassWetUnlit', this.CreateSimpleUnlitMaterial('Grass_Wet_Unlit_Mat', new B.Color3(0, .7, 0)));
+        this.materials.set('waterUnlit', this.CreateSimpleUnlitMaterial('Water_Unlit_Mat', new B.Color3(0, 0, 1)));
+        this.materials.set('sandUnlit', this.CreateSimpleUnlitMaterial('Grass_Unlit_Mat', new B.Color3(1, 1, .5)));
+        this.materials.set('stoneUnlit', this.CreateSimpleUnlitMaterial('Stone_Unlit_Mat', new B.Color3(.5, .5, .5)));
+        this.materials.set('lowMountainUnlit', this.CreateSimpleUnlitMaterial('Low_Mountain_Unlit_Mat', new B.Color3(.3, .3, .3)));
+        this.materials.set('highMountainUnlit', this.CreateSimpleUnlitMaterial('High_Mountain_Unlit_Mat', new B.Color3(.2, .2, .2)));
+        this.materials.set('snowUnlit', this.CreateSimpleUnlitMaterial('Snow_Unlit_Mat', new B.Color3(1, 1, .8)));
+        this.materials.set('PlayerDefaultUnlit', this.CreateSimpleUnlitMaterial('Player_Default_Unlit_Mat', new B.Color3(1, 0, 0)));
 
 
         // --- STREET
-        this.materials.set('fundoUnlit', this.CreateFundoUnLitMaterial());
-        this.materials.set('verticalStraightStreetUnlit', this.CreateVerticalStraightStreetUnLitMaterial());
-        this.materials.set('horizontalStraightStreetUnlit', this.CreateHorizontalStraightStreetUnLitMaterial());
-        this.materials.set('rightUpStreetUnlit', this.CreateRightUpStreetUnLitMaterial());
-        this.materials.set('rightDownStreetUnlit', this.CreateRightDownStreetUnLitMaterial());
-        this.materials.set('leftUpStreetUnlit', this.CreateLeftUpStreetUnLitMaterial());
-        this.materials.set('leftDownStreetUnlit', this.CreateLeftDownStreetUnLitMaterial());
-        this.materials.set('crossStreetUnlit', this.CreateCrossStreetUnLitMaterial());
+        this.materials.set('fundoUnlit', this.CreateTexturedUnlitMaterial('Fundo_Unlit_Mat', "./assets/textures/street/Fundo.png"));
+        
+        this.materials.set('verticalStraightStreetUnlit', this.CreateTexturedUnlitMaterial('Estrada_Linha_Reta_Vertical_Unlit_Mat', "./assets/textures/street/LinhaRetaVertical.png"));
+        this.materials.set('horizontalStraightStreetUnlit', this.CreateTexturedUnlitMaterial('Estrada_Linha_Reta_Horizontal_Unlit_Mat', "./assets/textures/street/LinhaRetaHorizontal.png"));
+        this.materials.set('rightUpStreetUnlit', this.CreateTexturedUnlitMaterial('Estrada_Direita_Cima_Unlit_Mat', "./assets/textures/street/DireitaCima.png"));
+        this.materials.set('rightDownStreetUnlit', this.CreateTexturedUnlitMaterial('Estrada_Direita_Baixo_Unlit_Mat', "./assets/textures/street/DireitaBaixo.png"));
+        this.materials.set('leftUpStreetUnlit', this.CreateTexturedUnlitMaterial('Estrada_Esquerda_Cima_Unlit_Mat', "./assets/textures/street/EsquerdaCima.png"));
+        this.materials.set('leftDownStreetUnlit', this.CreateTexturedUnlitMaterial('Estrada_Esquerda_Baixo_Unlit_Mat', "./assets/textures/street/EsquerdaBaixo.png"));
+        this.materials.set('crossStreetUnlit', this.CreateTexturedUnlitMaterial('Estrada_Cruz_Unlit_Mat', "./assets/textures/street/Cruz.png"));
         
         this.isInitialized = true;
 
     }
 
     
-    public CreateDefaultUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Default_Unlit_Mat', this.scene);
+    public CreateSimpleUnlitMaterial(name: string, color: B.Color3) : B.StandardMaterial {
+        
+        const mat = new B.StandardMaterial(name, this.scene);
         mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(1, 1, 1);
+        mat.emissiveColor = color;
 
         return mat;
     }
 
-
-    public CreateGrassUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Grass_Unlit_Mat', this.scene);
+    public CreateTexturedUnlitMaterial(name: string, path: string) : B.StandardMaterial {
+        
+        const mat = new B.StandardMaterial(name, this.scene);
         mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(0, 1, 0);
-
-        return mat;
-    }
-
-    public CreateGrassWetUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Grass_Wet_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(0, .7, 0);
-
-        return mat;
-    }
-
-    public CreateWaterUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Water_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(0, 0, 1);
-
-        return mat;
-    }
-
-    public CreateSandUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Sand_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(1, 1, .5);
-
-        return mat;
-    }
-
-
-    public CreateStoneUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Stone_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(.5, .5, .5);
-
-        return mat;
-    }
-
-    public CreateLowMountainUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Low_Mountain_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(.3, .3, .3);
-
-        return mat;
-    }
-
-    public CreateHighMountainUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('High_Mountain_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(.2, .2, .2);
-
-        return mat;
-    }
-
-    public CreateSnowUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Snow_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(1, 1, .8);
-
-        return mat;
-    }
-
-
-    public CreatePlayerDefaultUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Player_Default_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        mat.emissiveColor = new B.Color3(1, 0, 0);
-
-        return mat;
-    }
-
-
-    public CreateFundoUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Fundo_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        const emissiveTex = new B.Texture("./assets/textures/street/Fundo.png", this.scene);
-        mat.emissiveTexture = emissiveTex;
-
-        return mat;
-    }
-
-    public CreateVerticalStraightStreetUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Estrada_Linha_Reta_Vertical_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        const emissiveTex = new B.Texture("./assets/textures/street/LinhaRetaVertical.png", this.scene);
+        const emissiveTex = new B.Texture(path, this.scene);
         mat.emissiveTexture = emissiveTex;
 
         mat.emissiveTexture.wrapU = B.Texture.CLAMP_ADDRESSMODE;
         mat.emissiveTexture.wrapV = B.Texture.CLAMP_ADDRESSMODE;
 
         return mat;
+
     }
-
-    public CreateHorizontalStraightStreetUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Estrada_Linha_Reta_Horizontal_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        const emissiveTex = new B.Texture("./assets/textures/street/LinhaRetaHorizontal.png", this.scene);
-        mat.emissiveTexture = emissiveTex;
-
-        mat.emissiveTexture.wrapU = B.Texture.CLAMP_ADDRESSMODE;
-        mat.emissiveTexture.wrapV = B.Texture.CLAMP_ADDRESSMODE;
-
-        return mat;
-    }
-
-    public CreateRightUpStreetUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Estrada_Direita_Cima_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        const emissiveTex = new B.Texture("./assets/textures/street/DireitaCima.png", this.scene);
-        mat.emissiveTexture = emissiveTex;
-
-        mat.emissiveTexture.wrapU = B.Texture.CLAMP_ADDRESSMODE;
-        mat.emissiveTexture.wrapV = B.Texture.CLAMP_ADDRESSMODE;
-
-        return mat;
-    }
-
-
-    public CreateRightDownStreetUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Estrada_Direita_Baixo_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        const emissiveTex = new B.Texture("./assets/textures/street/DireitaBaixo.png", this.scene);
-        mat.emissiveTexture = emissiveTex;
-
-        mat.emissiveTexture.wrapU = B.Texture.CLAMP_ADDRESSMODE;
-        mat.emissiveTexture.wrapV = B.Texture.CLAMP_ADDRESSMODE;
-
-        return mat;
-    }
-
-
-    public CreateLeftUpStreetUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Estrada_Esquerda_Cima_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        const emissiveTex = new B.Texture("./assets/textures/street/EsquerdaCima.png", this.scene);
-        mat.emissiveTexture = emissiveTex;
-
-        mat.emissiveTexture.wrapU = B.Texture.CLAMP_ADDRESSMODE;
-        mat.emissiveTexture.wrapV = B.Texture.CLAMP_ADDRESSMODE;
-
-        return mat;
-    }
-
-    public CreateLeftDownStreetUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Estrada_Esquerda_Baixo_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        const emissiveTex = new B.Texture("./assets/textures/street/EsquerdaBaixo.png", this.scene);
-        mat.emissiveTexture = emissiveTex;
-
-        mat.emissiveTexture.wrapU = B.Texture.CLAMP_ADDRESSMODE;
-        mat.emissiveTexture.wrapV = B.Texture.CLAMP_ADDRESSMODE;
-
-        return mat;
-    }
-
-    public CreateCrossStreetUnLitMaterial() : B.StandardMaterial {
-
-        const mat = new B.StandardMaterial('Estrada_Cruz_Unlit_Mat', this.scene);
-        mat.disableLighting = true;
-        const emissiveTex = new B.Texture("./assets/textures/street/Cruz.png", this.scene);
-        mat.emissiveTexture = emissiveTex;
-
-        mat.emissiveTexture.wrapU = B.Texture.CLAMP_ADDRESSMODE;
-        mat.emissiveTexture.wrapV = B.Texture.CLAMP_ADDRESSMODE;
-
-        return mat;
-    }
-
 
     public GetMaterial(name : string) : B.StandardMaterial {
         if (!this.isInitialized)
